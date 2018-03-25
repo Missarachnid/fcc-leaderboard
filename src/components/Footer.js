0baae04ad7c3e69e4410b7a140c8929da1fcec40
Melissa Kepler <mmkepler@yahoo.com>
2018-03-25 16:15:02 -0400

    added untracked

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
     return (
       <Navbar className='footer navbar-fixed-bottom'>
         <a href='http://www.mmkepler.com/' className='melissa' target='blank'><b>Melissa Kepler</b></a>
-        <a href='https://github.com/Missarachnid' className='github' target='blank'><b>GitHub</b></a>
+        <a href='https://missarachnid.github.io/fcc-leaderboard/' className='github' target='blank'><b>GitHub</b></a>
       </Navbar>
     );
   }
